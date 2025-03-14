6440a57554d96951947baedd79ca3f4a0ef2f824
Lennart Busch <lennart.busch@yahoo.de>
2025-03-14 16:55:30 +0100

    Add Logexport

--- Sources/SwiftLogLens/LogLens.swift
+++ Sources/SwiftLogLens/LogLens.swift
@@ -32,6 +32,26 @@ public struct LogLens: Sendable{
             }            
         }
     }
+    
+    public static func loadLogs(
+        _ category: (any LogCategory)? = nil,
+        since fetchDate: Date = Date().addingTimeInterval(-1 * 60 * 60 * 24),
+        _ completion: @Sendable @escaping ([OSLogEntry]) -> Void
+    ){
+        DispatchQueue.global(qos: .utility).async {
+            guard let store = try? OSLogStore(scope: LogLensConfig.storeScope) else {return}
+            var predicate: NSPredicate
+            if let category{
+                predicate = NSPredicate(format: "(subsystem == %@) && (category IN %@)", LogLensConfig.defaultSubSystem, [category.rawValue])
+            }
+            else{
+                predicate = NSPredicate(format: "subsystem == %@", LogLensConfig.defaultSubSystem)
+            }
+            let pos = store.position(date: fetchDate)
+            let osLogs = (try? store.getEntries(at: pos, matching: predicate).compactMap({$0})) ?? []
+            completion(osLogs)
+        }
+    }
 }
 
 
